d4dfe9544b38e1b4903be43516df7632ab2972c0
aid3n-dev <aid3n.dev@gmail.com>
2024-01-21 19:48:11 +0900

    feat. Add various types

--- src/deepCopy.js
+++ src/deepCopy.js
@@ -3,11 +3,38 @@ const deepCopy = (target) => {
     return target;
   }
 
+  if (target instanceof Date) {
+    return new Date(target.getTime());
+  }
+
+  if (target instanceof RegExp) {
+    return new RegExp(target);
+  }
+
   if (Array.isArray(target)) {
     const copy = [];
 
     for (let i = 0; i < target.length; i++) {
       copy[i] = deepCopy(target[i]);
+    }
+
+    return copy;
+  }
+
+  if (target instanceof Map) {
+    const copy = new Map();
+    for (let [key, value] of target) {
+      copy.set(key, deepCopy(value));
+    }
+
+    return copy;
+  }
+
+  if (target instanceof Set) {
+    const copy = new Set();
+
+    for (let value of target) {
+      copy.add(deepCopy(value));
     }
 
     return copy;
