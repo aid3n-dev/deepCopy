7c36bde5b5dbf75697b4b71e7a20ffadf25302ab
aid3n-dev <aid3n.dev@gmail.com>
2024-01-21 17:03:12 +0900

    feat. Add deepcopy object & array

--- src/deepCopy.js
+++ src/deepCopy.js
@@ -1,3 +1,31 @@
-const deepCopy = (target) => {};
+const deepCopy = (target) => {
+  if (typeof target !== "object") {
+    return target;
+  }
+
+  if (Array.isArray(target)) {
+    const copy = [];
+
+    for (let i = 0; i < target.length; i++) {
+      copy[i] = deepCopy(target[i]);
+    }
+
+    return copy;
+  }
+
+  if (target instanceof Object) {
+    const copy = {};
+
+    for (const key in target) {
+      if (target.hasOwnProperty(key)) {
+        copy[key] = deepCopy(target[key]);
+      }
+    }
+
+    return copy;
+  }
+
+  return target;
+};
 
 module.exports = deepCopy;
